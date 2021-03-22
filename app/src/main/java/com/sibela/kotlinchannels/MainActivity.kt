fd4cd047374822b59142b1661a581a508ec8c877
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-22 17:01:51 -0300

    Implementing MainActivity

--- app/src/main/java/com/sibela/kotlinchannels/MainActivity.kt
+++ app/src/main/java/com/sibela/kotlinchannels/MainActivity.kt
@@ -2,9 +2,15 @@ package com.sibela.kotlinchannels
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
 import com.sibela.kotlinchannels.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,5 +18,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.eventFlow.collect { event ->
+                when (event) {
+                    is MainViewModel.Event.ErrorEvent -> {
+                        Snackbar.make(binding.root, event.message, Snackbar.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+
+        binding.btnShowSnackbar.setOnClickListener {
+            viewModel.triggerEvent()
+        }
     }
 }
